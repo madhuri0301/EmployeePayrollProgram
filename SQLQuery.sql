b3d9d3e012e558066b5d937b3ce0688faf5e3a02
Madhuri <harshupatil0301@gmail>
2021-06-29 19:06:45 +0530

    [Madhuri]Add : Inserting Data in Table

--- SQLQuery.sql
+++ SQLQuery.sql
@@ -9,3 +9,10 @@ employee_Name varchar(255),
 salary float,
 StartDate date
 );
+
+INSERT INTO Employee_Payroll(employee_Name,Salary,StartDate)
+VALUES ('Chetana PAtil', 15000.00 , '2021-03-14'),
+        ('Durgesh Patil',25000.00, '2021-12-12'),
+		('Madhuri Patil',30000.00, '2021-01-03'),
+		('Suhas Patil',22000.00, '2021-12-23'),
+		('Krishna Patil',20000.00, '2021-06-07');
