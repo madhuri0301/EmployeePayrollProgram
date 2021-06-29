28b23c4d0cf6bca5a800223625ab110546fac16e
Madhuri <harshupatil0301@gmail>
2021-06-29 18:59:57 +0530

    [Madhuri]Add : Creating Table As Employee Payroll

--- SQLQuery.sql
+++ SQLQuery.sql
@@ -2,3 +2,10 @@ CREATE DATABASE Payroll_Service
 
 USE Payroll_Service
 
+create Table Employee_Payroll
+(
+employee_ID int Identity(1,1) primary key,
+employee_Name varchar(255),
+salary float,
+StartDate date
+);
